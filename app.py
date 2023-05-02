5fa651e397d0998b2b22ba0061d9b568f9d98a93
Thomas Curran <tcurran@cavs.com>
2023-05-02 13:52:28 -0400

    Setup chat bot

---
+++ app.py
@@ -0,0 +1,19 @@
+import openai
+import toml 
+
+with open('secrets.toml', 'r') as f:
+    config = toml.load(f)
+
+openai.api_key = config['OPENAI_KEY']
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+
+print(response)
